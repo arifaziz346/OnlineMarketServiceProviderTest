1b407a76648d252e194f90b101860f94c4ac11b0
arifaziz346 <arifaziz346>
2021-07-23 02:58:49 -0700

    commit message

--- app/src/main/java/com/example/onlinemarketserviceprovider/Auth/login.kt
+++ app/src/main/java/com/example/onlinemarketserviceprovider/Auth/login.kt
@@ -1,12 +1,22 @@
 package com.example.onlinemarketserviceprovider.Auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import com.example.onlinemarketserviceprovider.R
+import com.example.onlinemarketserviceprovider.databinding.ActivityLoginBinding
 
 class login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_login)
+
+        binding.txtSignUp.setOnClickListener(View.OnClickListener {
+            var intent = Intent(this,register::class.java)
+            startActivity(intent)
+        })
     }
 }
--- app/src/main/java/com/example/onlinemarketserviceprovider/Auth/register.kt
+++ app/src/main/java/com/example/onlinemarketserviceprovider/Auth/register.kt
@@ -1,14 +1,22 @@
 package com.example.onlinemarketserviceprovider.Auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.onlinemarketserviceprovider.MainActivity
 import com.example.onlinemarketserviceprovider.R
+import com.example.onlinemarketserviceprovider.databinding.ActivityRegisterBinding
 
 class register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_register)
 
-
+    binding.btnSignUp.setOnClickListener(View.OnClickListener {
+        var intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+    })
     }
 }
